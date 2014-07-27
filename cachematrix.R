88e8324ecf3eabfd68830d393898e76b6cf126b0
Coursera-rprog-005-001 <ki.se-courses@retrapur.fr>
2014-07-27 12:31:10 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function is used to instanciate a matrix object.
+## In order to not waste resources the inverse of this matrix
+## will only be computed once, then cached thanks to the function "cacheSolve".
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## taking a matrix as argument this function will cache the inverse of said matrix.
+makeCacheMatrix <- function(M = matrix()) {
+  M.Inv <- NULL # The inverse is set to NULL for now
+  set <- function(y) {
+    # "<<-" is a super-assignment.
+    # Here it creates a varaibles in the global environment.
+    M <<- y
+    M.Inv <<- NULL
+  }
+  #Retun the matrix
+  get <- function() M
+  #Set the inverse of the matrix
+  setInv <- function(Inv) M.Inv <<- Inv
+  #Return the Inverse of the matrix
+  getInv <- function() M.Inv
+  #The function returns a list of objects (functions) to be used.
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Takes as argument an object from the function "makeCacheMatrix".
+## Solves the inverse of a matrix, caching the result in the above function.
+cacheSolve <- function(M, ...) {
+  M.Inv <- M$getInv() # The inverse solution is the value from the former function.
+  #If the value is not NULL, the inverse has been solved it is simply returned.
+  if(!is.null(M.Inv)) {
+    message("getting cached data")
+    return(M.Inv)
+  }
+  data <- M$get() #Get the original matrix
+  M.Inv <- solve(data, ...) #Solve  the inverse of the matrix.
+  M$setInv(M.Inv) #Set the object Inv value as the new inverse solution.
+  M.Inv #Return the Inversed matrix
 }
